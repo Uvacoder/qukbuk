671f280f77166725896860a8a5bfe1736f133a75
Oz Ertas <74022564+oerts@users.noreply.github.com>
2022-03-12 00:32:12 +0300

    build(web): replace package manager

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,7 @@ import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: adapter(),
-		target: '#svelte'
+		adapter: adapter()
 	},
 
 	preprocess: [
